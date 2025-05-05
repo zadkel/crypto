4d3208954a8b29bb5f2acf4dbf4999e69b49f9a0
zadkel <45797960+zadkel@users.noreply.github.com>
2025-05-06 03:59:55 +0900

    밀러라빈

--- 밀러라빈.py
+++ 밀러라빈.py
@@ -37,3 +37,16 @@ def PrimeGen(n):
 
 for i in range(30):
   print(PrimeGen(10))
+
+
+'''
+밀러라빈에 사용되는 정리.
+
+만약 N이 홀수거나, 퍼펙트 스퀘어가 아닌 경우. Zn* 의 원소 중 적어도 절반은 강하게 합성수 테스트를 통과함. (즉, a^u != +-1, or a^2k u != -1 (mod N) for all i)
+
+(x^16+1) = (x^8+1)(x^4+1)(x^2+1)(x+1)(x-1) =0 mod N 이런 식으로 인수분해 되니까
+
+-> B = set of a s.t. a^2iu = +-1 B' = set of a s.t a^2iu = -1
+
+그러면 B⊂B' 이때, B'는 Zn의 strrictly subgroup이므로 |B|<=|B'|<|Zn|/2
+'''
